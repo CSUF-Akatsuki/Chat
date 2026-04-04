4bb95d1e61a5c80ef44d12f9182b61fecd740fbe
William L <frewtypebbles@gmail.com>
2026-04-04 16:34:46 -0700

    made functions async, added logout auth function.

--- backend/auth_lambda.py
+++ backend/auth_lambda.py
@@ -1,9 +1,38 @@
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
+from shared.auth import login, register, refresh_session, confirm_registration
 
 
-
-def handler(event:dict, context:LambdaContext):
+def main(event:dict, context:LambdaContext):
     api_event = APIGatewayProxyEvent(event)
     
-    
+    match api_event.http_method:
+        case "GET":
+            return get(api_event, context)
+        
+        case "POST":
+            return post(api_event, context)
+        
+        case "PUT":
+            return put(api_event, context)
+        
+        case "PATCH":
+            return patch(api_event, context)
+        
+        case "DELETE":
+            return delete(api_event, context)
+
+def get(api_event:APIGatewayProxyEvent, context:LambdaContext):
+    pass
+
+def post(api_event:APIGatewayProxyEvent, context:LambdaContext):
+    pass
+
+def put(api_event:APIGatewayProxyEvent, context:LambdaContext):
+    pass
+
+def patch(api_event:APIGatewayProxyEvent, context:LambdaContext):
+    pass
+
+def delete(api_event:APIGatewayProxyEvent, context:LambdaContext):
+    pass
--- backend/shared/auth.py
+++ backend/shared/auth.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
 from typing import Literal
+from models.users_model import CreateUserRequest, User
 from shared.logger import logger
 from pycognito import Cognito
 from pycognito.utils import TokenType
 from shared.config import AWS_COGNITO_CLIENT_ID, AWS_COGNITO_USER_POOL_ID, AWS_COGNITO_CLIENT_SECRET
 from botocore.exceptions import ClientError
-
+from shared.db.database import db_connection
 # Initialize the library
 
 @dataclass
@@ -14,7 +15,7 @@ class AuthData:
     access_token:TokenType
     refresh_token:str
 
-def login(username:str, password:str) -> None | AuthData:
+async def login(username:str, password:str) -> None | AuthData:
     try:
         u = Cognito(
             user_pool_id=AWS_COGNITO_USER_POOL_ID, 
@@ -35,7 +36,7 @@ def login(username:str, password:str) -> None | AuthData:
         logger.exception(f"Unexpected error durring login: {e}")
         return None
     
-def refresh_session(refresh_token: str) -> None | AuthData:
+async def refresh_session(refresh_token: str) -> None | AuthData:
     try:
         u = Cognito(
             user_pool_id=AWS_COGNITO_USER_POOL_ID,
@@ -58,35 +59,73 @@ def refresh_session(refresh_token: str) -> None | AuthData:
         return None
 
 
-def register(email:str, username:str, password:str):
-    u = Cognito(
-        user_pool_id=AWS_COGNITO_USER_POOL_ID, 
-        client_id=AWS_COGNITO_CLIENT_ID,
-        client_secret=AWS_COGNITO_CLIENT_SECRET,
-        username=username
-    )
-
+async def register(user: CreateUserRequest):
     try:
-        u.set_base_attributes(email=email)
-        u.register(username, password)
-        logger.info(f"""User "{username}" successfully started account registration process.""")
+        u = Cognito(
+            user_pool_id=AWS_COGNITO_USER_POOL_ID, 
+            client_id=AWS_COGNITO_CLIENT_ID,
+            client_secret=AWS_COGNITO_CLIENT_SECRET,
+            username=user.username
+        )
+        u.set_base_attributes(email=user.email)
+        u.register(user.username, user.password)
+        logger.info(f"""User "{user.username}" successfully started account registration process.""")
     except ClientError as e:
-        logger.error(f'Registration failed for "{username}": {e.response["Error"]["Message"]}')
+        logger.error(f'Registration failed for "{user.username}": {e.response["Error"]["Message"]}')
     except Exception as e:
         logger.exception(f"Unexpected error during registration: {e}")
 
-def confirm_registration(username:str, code:str):
-    u = Cognito(
-        user_pool_id=AWS_COGNITO_USER_POOL_ID, 
-        client_id=AWS_COGNITO_CLIENT_ID,
-        client_secret=AWS_COGNITO_CLIENT_SECRET,
-        username=username
-    )
-
+async def confirm_registration(email:str, username:str, code:str) -> User | None:
     try:
+        u = Cognito(
+            user_pool_id=AWS_COGNITO_USER_POOL_ID, 
+            client_id=AWS_COGNITO_CLIENT_ID,
+            client_secret=AWS_COGNITO_CLIENT_SECRET,
+            username=username
+        )
         u.confirm_sign_up(code, username=username)
+        query = """
+            INSERT INTO users (email, username)
+            VALUES (:email, :username)
+            RETURNING id, email, username, created_at
+        """
+        new_user = await db_connection.fetch_one(
+            query=query,
+            values={
+                "email": email,
+                "username": username,
+            },
+        )
         logger.info(f"""User "{username}" successfully confirmed their account creation.""")
+        return User(
+            id=new_user["id"], username=new_user["username"], email=new_user["email"]
+        )
     except ClientError as e:
         logger.error(f'Confirming registration failed for "{username}": {e.response["Error"]["Message"]}')
+        return None
     except Exception as e:
         logger.exception(f"Unexpected error during registration confirmation: {e}")
+        return None
+    
+async def logout(refresh_token: str):
+    """
+    Revokes the specific refresh token and its associated access tokens.
+    """
+    try:
+        u = Cognito(
+            user_pool_id=AWS_COGNITO_USER_POOL_ID,
+            client_id=AWS_COGNITO_CLIENT_ID,
+            client_secret=AWS_COGNITO_CLIENT_SECRET
+        )
+        
+        u.client.revoke_token(
+            Token=refresh_token,
+            ClientId=AWS_COGNITO_CLIENT_ID,
+            ClientSecret=AWS_COGNITO_CLIENT_SECRET
+        )
+        
+        logger.info("User successfully logged out of this session.")
+        return True
+    except ClientError as e:
+        logger.error(f"Logout failed: {e.response['Error']['Message']}")
+        return False
